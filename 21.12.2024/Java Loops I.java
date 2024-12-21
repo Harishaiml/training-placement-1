d9e1ae732c987f9f33d48056ccc3997b78e37a8b
Harishaiml <147464547+Harishaiml@users.noreply.github.com>
2024-12-21 20:05:07 +0530

    Create Java Loops I.java

---
+++ 21.12.2024/Java Loops I.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Solution{
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
+
+        int N=Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int i=1; i <= 10; i++){
+            int value=N * i;
+            System.out.println(N+" x "+i+"="+value);
+        }
+
+        bufferedReader.close(); 
+    }
+}
